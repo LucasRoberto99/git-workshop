23b5a09bc8451d48534c55b12590e3c63a9b5f6e
Lucas Roberto <lucasr.prof@gmail.com>
2025-11-28 11:20:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+        necessitatibus sunt suscipit nobis beatae reprehenderit ex officiis
+        aliquid laboriosam deleniti aut amet dolores reiciendis quisquam
+        repellendus, quos explicabo! Possimus, voluptatibus.
+      </p>
+      <button>Test</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Titre de mon site !</h1>
+    </div>
+  );
+};
+
+export default Header;
