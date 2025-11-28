0c96fe41a62757d4a1a99b7184ff6dd610af07f0
Lucas Roberto <lucasr.prof@gmail.com>
2025-11-28 11:43:54 +0100

    caps header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ const Header = () => {
   return (
     <div>
       <h1>
-        Titre de mon <span style={{ color: "red" }}>site</span> !
+        CECI EST LE TITRE DE LA BRANCHE MASTER{" "}
+        <span style={{ color: "red" }}>site</span> !
       </h1>
     </div>
   );
